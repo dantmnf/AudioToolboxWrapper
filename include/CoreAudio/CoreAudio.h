aa80b586f49ca75aefd51f8c4088aeebfe99f99a
maz-1 <ohmygod19993@gmail.com>
2018-08-14 23:10:42 +0800

    add CoreAudio.h

---
+++ include/CoreAudio/CoreAudio.h
@@ -0,0 +1,23 @@
+#ifndef __CoreAudio_h__
+#define __CoreAudio_h__
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmultichar"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wfour-char-constants"
+#endif
+
+#define noErr 0
+#define kAudioFormatFlagsNativeEndian 0
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#endif
