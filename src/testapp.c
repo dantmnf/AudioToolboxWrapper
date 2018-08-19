d8d18ea03c77bbce2291bb14dc36d40b1af6090c
dantmnf <dantmnf2@gmail.com>
2018-08-20 00:41:36 +0800

    update testapp

--- src/testapp.c
+++ src/testapp.c
@@ -15,9 +15,19 @@ int main() {
   AudioStreamBasicDescription out_format = {
        .mSampleRate = 44100,
        .mFormatID = kAudioFormatLinearPCM,
-       .mChannelsPerFrame = in_format.mChannelsPerFrame,
+       .mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked,
+       .mBytesPerPacket = 4,
+       .mFramesPerPacket = 1,
+       .mBytesPerFrame = 4,
+       .mChannelsPerFrame = 2,
+       .mBitsPerChannel = 16,
   };
   AudioConverterRef converter;
   OSStatus ret = AudioConverterNew(&in_format, &out_format, &converter);
   printf("AudioConverterNew() => %d\n", ret);
+  if (ret) {
+    printf("failed\n");
+  } else {
+    printf("successfully created AudioConverter instance\n");
+  }
 }
